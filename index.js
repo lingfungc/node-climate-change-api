747ac6eeb3ba3be87d8224dd4b3ba09b0c1f92d0
Ling Fung Chan <justinlfchan@gmail.com>
2023-07-15 22:46:37 +0100

    npm installed axion express nodemon and setting up backend in index.js with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // * Create server for the application
+
+const PORT = 8000;
+
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// * We are getting all the features/packages from "express"
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
