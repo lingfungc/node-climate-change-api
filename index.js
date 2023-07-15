eed76be29d89149d6d84a5c598eed90ebd9ceb18
Ling Fung Chan <justinlfchan@gmail.com>
2023-07-15 23:44:29 +0100

    added app.get('/') and app.get('/news') routes and actions with express

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ const cheerio = require("cheerio");
 // * We are getting all the features/packages from "express"
 const app = express();
 
+// * Setting up the routes and the actions for each routes
+app.get("/", (req, res) => {
+  // This ".json()" is converting the data from JSON to JavaScript object
+  res.json("Welcome to my Climate Change News API");
+});
+
+app.get("/news", (req, res) => {
+  axios
+    .get("https://www.nytimes.com/international/section/opinion")
+    .then((response) => {
+      const html = response.data;
+      console.log(html);
+    });
+});
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
