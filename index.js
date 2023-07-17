10076d18f6fa5e33ba0683410ec52267aabab913
Ling Fung Chan <justinlfchan@gmail.com>
2023-07-17 11:43:13 +0100

    created getArticles() to scrape climate-change data for each newspaper

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ const cheerio = require("cheerio");
 // * We are getting all the features/packages from "express"
 const app = express();
 
+const getArticles = (newspaperData) => {
+  axios.get(newspaperData.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $("a", html).each(function () {
+      const title = $(this).text();
+      // console.log(title);
+
+      // const url = $(this).attr("href");
+
+      if (title.includes("Climate") || title.includes("climate")) {
+        let url = $(this).attr("href");
+        if (!url.includes("https")) {
+          url = nytimesUrl + url;
+        }
+
+        articles.push({
+          source: newspaperData.name,
+          title,
+          url,
+        });
+      }
+    });
+  });
+};
+
 const nytimesUrl = "https://www.nytimes.com";
 
 const newspapers = [
@@ -32,6 +59,10 @@ const newspapers = [
 
 const articles = [];
 
+newspapers.forEach((newspaper) => {
+  getArticles(newspaper);
+});
+
 // * Setting up the routes and the actions for each routes
 app.get("/", (req, res) => {
   // This ".json()" is converting the data from JSON to JavaScript object
@@ -39,36 +70,40 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
-  axios
-    .get("https://www.nytimes.com/international/section/climate")
-    .then((response) => {
-      // * Fetch the HTML file from the URL
-      const html = response.data;
-      // console.log(html);
-
-      // * We use cheerio to pick up elements from the HTML file, and "$" is a cheerio syntax
-      const $ = cheerio.load(html);
-
-      $("a:contains('Climate')", html).each(function () {
-        const title = $(this).text();
-        console.log(title);
-
-        let url = $(this).attr("href");
-        if (!url.includes("https")) {
-          url = nytimesUrl + url;
-        }
-        console.log(url);
-
-        articles.push({
-          title,
-          url,
-        });
-      });
-      res.json(articles);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  res.json(articles);
 });
+
+// app.get("/news", (req, res) => {
+//   axios
+//     .get("https://www.nytimes.com/international/section/climate")
+//     .then((response) => {
+//       // * Fetch the HTML file from the URL
+//       const html = response.data;
+//       // console.log(html);
+
+//       // * We use cheerio to pick up elements from the HTML file, and "$" is a cheerio syntax
+//       const $ = cheerio.load(html);
+
+//       $("a:contains('Climate')", html).each(function () {
+//         const title = $(this).text();
+//         console.log(title);
+
+//         let url = $(this).attr("href");
+//         if (!url.includes("https")) {
+//           url = nytimesUrl + url;
+//         }
+//         console.log(url);
+
+//         articles.push({
+//           title,
+//           url,
+//         });
+//       });
+//       res.json(articles);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
